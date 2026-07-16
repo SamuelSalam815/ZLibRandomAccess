72be936f8967c5d75626bfa757176cb5d37f4989
Sam Salam <samuelsalam815@gmail.com>
2026-07-16 04:58:22 +0100

    Integrate building zlib from source on each build

--- ZLibBindings/ZLibLowLevelBindings.cs
+++ ZLibBindings/ZLibLowLevelBindings.cs
@@ -3,10 +3,14 @@ using System.Runtime.InteropServices;
 
 namespace ZLibBindings;
 
+using unsafe voidpf = void**;
+using unsafe Bytef = byte*;
 
 public static partial class ZLibLowLevelBindings
 {
-    [LibraryImport("zlib1.dll")]
+    private const string ZlibLibrary = "z.dll";
+
+    [LibraryImport(ZlibLibrary)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     private static partial IntPtr zlibVersion();
 
@@ -15,19 +19,24 @@ public static partial class ZLibLowLevelBindings
 }
 
 
+public unsafe delegate voidpf alloc_func(voidpf opaque, uint items, uint size);
+
+public unsafe delegate void free_func(voidpf opaque, voidpf address);
+
 public unsafe struct z_stream_s
 {
-    byte** next_in;
-    ushort avail_in;
+    Bytef *next_in;
+    uint avail_in;
     uint total_in;
 
-    byte** next_out;
-    ushort avail_out;
-    uint total_out;
+    Bytef *next_out;
+    uint avail_out;
+    ulong total_out;
 
     byte* msg;
-
     void** state;
 
+    IntPtr zalloc;
+    IntPtr zfree;
 
 }
