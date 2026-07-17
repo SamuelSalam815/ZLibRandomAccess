5bc6cbd9dc16906109ffe3ceaf3f91c4de90dcae
Sam Salam <samuelsalam815@gmail.com>
2026-07-17 10:44:44 +0100

    Improve zlibexception message

--- ZLibWrapper/ZLibException.cs
+++ ZLibWrapper/ZLibException.cs
@@ -5,7 +5,10 @@ namespace ZLibWrapper;
 public class ZLibException : Exception
 {
     public ZReturnCode ReturnCode { get; }
-    public ZLibException(string? message, ZReturnCode returnCode) : base(message)
+    public ZLibException(string? message, ZReturnCode returnCode) : base(
+        $"Encountered return code '{returnCode}' ({(int)returnCode})"
+        + (message is null ? string.Empty : $" with error message '{message}'")
+        )
     {
         ReturnCode = returnCode;
     }
