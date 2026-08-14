80da0ecbd62cc51ade53c03209c496041d5fa070
Sam Salam <samuelsalam815@gmail.com>
2026-08-14 09:52:16 +0100

    Revert "Remove redundant StreamReaderWithByteCountLimit.cs"
    This reverts commit f47cb3b6e6cb2be211d7b4758dc9a8f81743a202.

---
+++ ArchiveViewerBackend/StreamReaderWithByteCountLimit.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace ArchiveViewerBackend;
+
+// TODO: consider deleting
+public class StreamReaderWithByteCountLimit : StreamReaderWithByteCount
+{
+    private readonly int? _maximumNumberOfBytesRead;
+
+    public StreamReaderWithByteCountLimit(
+        Stream stream,
+        int bufferSize = 1024,
+        bool leaveOpen = false,
+        int? maximumNumberOfBytesRead = null) : base(
+        stream,
+        Encoding.Default,
+        bufferSize,
+        leaveOpen)
+    {
+        _maximumNumberOfBytesRead = maximumNumberOfBytesRead;
+    }
+
+    public StreamReaderWithByteCountLimit(
+        Stream stream,
+        Encoding encoding,
+        int bufferSize = 1024,
+        bool leaveOpen = false,
+        int? maximumNumberOfBytesRead = null) : base(stream, encoding, bufferSize, leaveOpen)
+    {
+        _maximumNumberOfBytesRead = maximumNumberOfBytesRead;
+    }
+
+    public override int Read()
+    {
+        if (NumberOfBytesRead >= _maximumNumberOfBytesRead)
+        {
+            return -1;
+        }
+        return base.Read();
+    }
+}
