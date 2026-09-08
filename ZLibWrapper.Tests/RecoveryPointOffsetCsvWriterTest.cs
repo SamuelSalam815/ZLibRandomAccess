e775818f0bc08c18a4184eaf1b3a70bf06e6fd29
Sam Salam <samuelsalam815@gmail.com>
2026-09-08 11:50:52 +0100

    Add class for writing csv of recovery points

---
+++ ZLibWrapper.Tests/RecoveryPointOffsetCsvWriterTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace ZLibWrapper.Tests;
+
+[TestClass]
+[TestSubject(typeof(RecoveryPointOffsetCsvWriter))]
+public class RecoveryPointOffsetCsvWriterTest
+{
+
+    [TestMethod]
+    [DataRow(true)]
+    [DataRow(false)]
+    public void StreamIsClosed_WhenDisposedAndLeaveOpenFalse(bool leaveOpen)
+    {
+        var stream = new MemoryStream();
+        var writeOperation = () => stream.Write([2]);
+
+        writeOperation.ShouldNotThrow();
+
+        new RecoveryPointOffsetCsvWriter(stream, Encoding.Default, leaveOpen: leaveOpen).Dispose();
+
+        if (leaveOpen)
+        {
+            writeOperation.ShouldNotThrow();
+        }
+        else
+        {
+            writeOperation.ShouldThrow<Exception>();
+        }
+    }
+
+    public static IEnumerable<object?[]> Encodings()
+    {
+        yield return [Encoding.Default];
+        yield return [Encoding.ASCII];
+        yield return [Encoding.Unicode];
+        yield return [Encoding.UTF8];
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(Encodings))]
+    public void WritingUsingCsvHelper_ProducesExpectedResult(Encoding encoding)
+    {
+        var records = new List<RecoveryPointOffset> { new(1,2), new(3,4), };
+        var expectedResult =
+            $"""
+             {nameof(RecoveryPointOffset.OffsetInUncompressedStream)},{nameof(RecoveryPointOffset.OffsetInCompressedStream)}
+             1,2
+             3,4
+
+             """;
+        using var memoryStream = new MemoryStream();
+        using (var sut = new RecoveryPointOffsetCsvWriter(memoryStream, encoding, leaveOpen: true))
+        {
+            sut.WriteAll(records);
+        }
+        memoryStream.Position = 0;
+        using var streamReader = new StreamReader(memoryStream, encoding);
+        var actualResult = streamReader.ReadToEnd();
+
+        actualResult.ShouldBe(expectedResult);
+    }
+}
---
+++ ZLibWrapper/RecoveryPointOffsetCsvWriter.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System.Text;
+using CsvHelper;
+
+namespace ZLibWrapper;
+
+public class RecoveryPointOffsetCsvWriter : IDisposable, IAsyncDisposable
+{
+    private readonly CsvWriter _csvWriter;
+
+    public RecoveryPointOffsetCsvWriter(Stream outputStream, Encoding encoding, bool leaveOpen = false)
+    {
+        var streamWriter = new StreamWriter(outputStream, encoding, leaveOpen: leaveOpen);
+        _csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
+        _csvWriter.WriteHeader<RecoveryPointOffset>();
+        _csvWriter.NextRecord();
+    }
+
+    public void Dispose()
+    {
+        _csvWriter.Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _csvWriter.DisposeAsync();
+    }
+
+    public void WriteAll(IEnumerable<RecoveryPointOffset> records)
+    {
+        foreach (var record in records)
+        {
+            Write(record);
+        }
+    }
+
+    public void Write(RecoveryPointOffset record)
+    {
+        _csvWriter.WriteRecord(record);
+        _csvWriter.NextRecord();
+    }
+}
